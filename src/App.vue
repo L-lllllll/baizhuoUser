37dc61ca529b968aeaebf9a41bc4cb56a9398f4d
quantao <3197835434@qq.com>
2023-07-05 16:29:46 +0800

    选择登录样式

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,12 @@
 
 <style>
 	/*每个页面公共css */
+button {
+	background: none !important;
+	border: 1px solid #fe402a !important;
+}
+button[type=primary] {
+  background: #fe402a !important;
+	border: none !important;
+}
 </style>
--- src/pages/login_rpk/login.vue
+++ src/pages/login_rpk/login.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="login">login</div>
+  <view>
+    <text class="h2">供应链用户小程序</text>
+    <view class="logo"></view>
+    <div class="btn">
+      <button class="button" type="primary">微信快捷登录</button>
+      <button class="button" type="default">手机号登录</button>
+    </div>
+    <view class="user_agreement">
+      <label class="radio">
+        <radio value="r2" color="#fe402a" />
+        <text>我已阅读并同意</text>
+        <navigator href="#">
+          《用户服务协议》
+        </navigator>
+        <text>和</text>
+        <navigator href="#">《隐私政策》</navigator>
+      </label>
+    </view>
+  </view>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue';
 export default Vue.extend({
   components: {},
@@ -34,4 +52,49 @@ export default Vue.extend({
 }) 
 </script>
 
-<style scoped></style>
+<style scoped>
+.h2 {
+  display: block;
+  font-size: 42rpx;
+  font-weight: 700;
+  text-align: center;
+  margin: 80rpx 0;
+}
+.logo {
+  width: 500rpx;
+  height: 150rpx;
+  background: #fe402a;
+  margin: 0 auto 100rpx;
+}
+button {
+  width: 90%;
+  border-radius: 50rpx;
+  font-size: 36rpx;
+  line-height: 2.5;
+  margin-bottom: 30rpx;
+}
+radio {
+  transform: scale(0.7);
+  margin-top: -4rpx;
+}
+.user_agreement  {
+  width: 100%;
+  position: absolute;
+  bottom: 100rpx;
+  left: 50%;
+  transform: translate(-50%);
+}
+.user_agreement label {
+  display: flex;
+  justify-content: center;
+}
+navigator {
+  color: #fe402a;
+}
+button[type=default]:active {
+  background: none !important;
+}
+button[type=primary]:active {
+  background-color: #fe402a;
+}
+</style>
